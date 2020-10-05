516b094f7e01074896cc695f1347ca15cb9fdb66
shishir75 <shishir.srdr16@gmail.com>
2020-10-05 22:55:46 +0600

    Understanding Children Property

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const person = (props) => {
     return (
-        <p>
-            I am a {props.name} and my age is {props.age}
-        </p>
+        <div>
+            <p>
+                I am a {props.name} and my age is {props.age}
+            </p>
+            <p>{props.children}</p>
+        </div>
     );
 };
 
