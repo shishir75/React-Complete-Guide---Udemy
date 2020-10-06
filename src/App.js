0a77d87d08b19a74339385d1c42056a8cc7c8053
shishir75 <shishir.srdr16@gmail.com>
2020-10-06 23:25:11 +0600

    Handling Dynamic Content - The JavaScript Way

--- src/App.js
+++ src/App.js
@@ -50,36 +50,38 @@ class App extends Component {
             borderRadius: "5px",
         };
 
+        let persons = null;
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}
+                    ></Person>
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, "Obydullah")}
+                        changed={this.nameChangedHandler}
+                    >
+                        My Hobby is Sleeping
+                    </Person>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}
+                    ></Person>
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I am react app</h1>
                 <p>Its really awesome...</p>
                 <button style={style} onClick={this.togglePersonHandler}>
-                    Switch Name
+                    Toggle Persons
                 </button>
-                {this.state.showPersons ? (
-                    <div>
-                        <Person
-                            name={this.state.persons[0].name}
-                            age={this.state.persons[0].age}
-                        ></Person>
-                        <Person
-                            name={this.state.persons[1].name}
-                            age={this.state.persons[1].age}
-                            click={this.switchNameHandler.bind(
-                                this,
-                                "Obydullah"
-                            )}
-                            changed={this.nameChangedHandler}
-                        >
-                            My Hobby is Sleeping
-                        </Person>
-                        <Person
-                            name={this.state.persons[2].name}
-                            age={this.state.persons[2].age}
-                        ></Person>
-                    </div>
-                ) : null}
+                {persons}
             </div>
         );
     }
